5adf25a47f95503b1513bce39d87b89e63db4d8a
masiejczyk.aleksandra@gmail.com <masiejczyk.aleksandra@gmail.com>
2025-04-26 12:29:47 +0200

    Init commit

---
+++ QAliti/wwwroot/js/scripts.js
@@ -0,0 +1,8 @@
+﻿// Funkcja do przewijania strony na górę
+function scrollToTop() {
+    const navbarHeight = document.querySelector('.top-navbar').offsetHeight;  // Pobierz wysokość navbaru
+    window.scrollTo({
+        top: 0 - navbarHeight,  // Zanim przewiniemy na górę, odejmujemy wysokość navbaru
+        behavior: 'smooth'      // Płynne przewijanie
+    });
+}
